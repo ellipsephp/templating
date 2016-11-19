aa644eb2528738005f1d7835ab1d1292a01ad929
pmall <pierre.mallinjoud@gmail.com>
2016-11-19 15:21:13 +0100

    adds a service provider automatically registering plates and twig engine

--- src/EngineFactory.php
+++ src/EngineFactory.php
@@ -6,15 +6,28 @@ class EngineFactory
 {
     private static $factories = [];
 
+    private function __construct()
+    {
+        //
+    }
+
     public static function register($name, array $config, callable $factory)
     {
-        static::$factories[$name] = [$factory, $config];
+        $views_dir = $config['views_dir'];
+        $options = $config['engines'][$name];
+
+        static::$factories[$name] = [$factory, $views_dir, $options];
+    }
+
+    public static function has($name)
+    {
+        return array_key_exists($name, static::$factories);
     }
 
     public static function make($name)
     {
-        list($factory, $config) = static::$factories[$name];
+        list($factory, $views_dir, $options) = static::$factories[$name];
 
-        return $factory($config);
+        return $factory($views_dir, $options);
     }
 }
---
+++ src/TemplateServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Pmall\Templating;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+use League\Plates\Engine as Plates;
+use Twig_Loader_Filesystem;
+
+use Pmall\Templating\Adapters\PlatesAdapter;
+use Pmall\Templating\Adapters\TwigAdapter;
+
+class TemplateServiceProvider extends AbstractServiceProvider
+{
+    const KEY_PLATES = 'plates';
+    const KEY_TWIG = 'twig';
+
+    private $config;
+
+    protected $provides = [
+        EngineInterface::class
+    ];
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function boot()
+    {
+        if ($this->shouldregisterEngine(static::KEY_PLATES)) {
+
+            EngineFactory::register(static::KEY_PLATES, $this->config, function ($view_dir, $options) {
+
+                return new PlatesAdapter(new Plates($views_dir));
+
+            });
+
+        }
+
+        if ($this->shouldregisterEngine(static::KEY_TWIG)) {
+
+            EngineFactory::register(static::KEY_TWIG, $this->config, function ($view_dir, $options) {
+
+                return new TwigAdapter(new Twig_Loader_Filesystem($views_dir), $options);
+
+            });
+
+        }
+    }
+
+    public function register()
+    {
+        $this->getContainer()->share(EngineInterface::class, function () {
+
+            $engine = $this->config['engine'];
+
+            return EngineFactory::make($engine);
+
+        });
+    }
+
+    private function shouldregisterEngine($engine)
+    {
+        return $this->config['engine'] == $engine and ! EngineFactory::has($engine);
+    }
+}

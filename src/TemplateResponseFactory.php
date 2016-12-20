ffaf6a358db1a02842d82b955da955732edf4a29
pmall <pierre.mallinjoud@gmail.com>
2016-12-20 20:51:08 +0100

    stop formatter concept and use http response factory

---
+++ src/TemplateResponseFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+use Pmall\Http\Factories\HtmlResponseFactory;
+
+class TemplateResponseFactory extends HtmlResponseFactory
+{
+    private $engine;
+
+    public function __construct(EngineInterface $engine)
+    {
+        $this->engine = $engine;
+    }
+
+    public function createResponse($code = 200, $file, array $data = [], array $headers = [])
+    {
+        $html = $this->engine->render($file, $data);
+
+        return parent::createResponse($code, $html, $headers);
+    }
+}

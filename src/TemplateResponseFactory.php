89a5fdc461035b5f0bae7e107a213f8cfe477065
pmall <pierre.mallinjoud@gmail.com>
2016-12-24 00:30:31 +0100

    allow default values to be set before creating a template response

--- src/TemplateResponseFactory.php
+++ src/TemplateResponseFactory.php
@@ -8,13 +8,22 @@ class TemplateResponseFactory extends HtmlResponseFactory
 {
     private $engine;
 
+    private $defaults = [];
+
     public function __construct(EngineInterface $engine)
     {
         $this->engine = $engine;
     }
 
+    public function set($key, $value)
+    {
+        $this->default[$key] = $value;
+    }
+
     public function createResponse($code = 200, $file = '', array $data = [], array $headers = [])
     {
+        $data = array_merge($this->defaults, $data);
+
         $html = $this->engine->render($file, $data);
 
         return parent::createResponse($code, $html, $headers);
